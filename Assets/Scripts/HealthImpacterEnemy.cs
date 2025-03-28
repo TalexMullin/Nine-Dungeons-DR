96e9f2f331de0e27ab7252a04dfbba87df815e08
talex52 <titus.mullin@my.uu.edu>
2025-03-28 16:21:43 -0500

    enemies have health and can be damaged

---
+++ Assets/Scripts/HealthImpacterEnemy.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class HealthImpacterEnemy : MonoBehaviour
+{
+    //public vars that can be adjusted in settings.
+    public bool enterOrNot = true; // determins if ontriggerenter or ontriggerstay runs.
+    public int healthChangeAmount = -5;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!PlayerController.gamePaused)
+        {
+            if (enterOrNot)
+            {
+                EnemyHealthController controller = collision.GetComponent<EnemyHealthController>();
+
+                if (controller != null)
+                {
+                    controller.ChangeHealth(healthChangeAmount);
+                }
+            }
+        }
+    }
+
+
+
+    /*
+     *
+     *
+     *
+     *
+     *
+     */
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!enterOrNot)
+        {
+            EnemyHealthController controller = collision.GetComponent<EnemyHealthController>();
+
+            if (controller != null)
+            {
+                controller.ChangeHealth(healthChangeAmount);
+            }
+        }
+    }
+}
